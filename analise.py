2d4a7ded21437ea15acda717e517b2acb5f397ba
Patrick Lima <patrickgdlima@gmail.com>
2021-07-11 22:54:09 -0300

    feat: adding matplot charts to better analyze

--- analise.py
+++ analise.py
@@ -6,6 +6,10 @@
 #%% Importar os pacotes de conexão ao BD
 import psycopg2
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+FS = (16, 8)  # figure size
 
 #%% Credenciais do Banco de Dados
 user = "postgres"
@@ -56,6 +60,8 @@ def get_data_from_db(query, df, col_names):
 col_names_city = get_column_names("cidade")
 col_names_resumo = get_column_names("resumo")
 
+print(col_names_city)
+
 #%% Criando um DF vazio + fazendo seleção das colunas e informações que fazem o append no DF
 query_df = pd.DataFrame()
 query = """
@@ -66,8 +72,106 @@ query = """
 """
 
 cols_with_joins = col_names_city + ['state_name'] + col_names_resumo
-
 query_df = get_data_from_db(query, query_df, cols_with_joins)
+
+#%% Filtrando pelas 5 cidades selecionadas
+query_df_city = pd.DataFrame()
+query = """
+    SELECT c.nome
+    FROM cidade c
+"""
+
+query_df_city = get_data_from_db(query, query_df_city, ['nome'])
+
+#%% Filtrando pela cidade e formatando a primeira coluna para a Data facilitando o plot
+def filter_and_format_by_city(city_name):
+    df_city_filter = query_df[query_df['nome'] == city_name]
+
+    df_city_filter.data = pd.to_datetime(df_city_filter.data, format="%Y-%m-%d")
+    df_city_filter.set_index("data", inplace=True)
+    df_city_filter.index.name = "Data"
+    return df_city_filter
+
+#%% Construção do gráfico diário com o cálculo das ultimas mortes e confirmados dividido pela população a cada 100000 habitantes
+
+def plot_df_by_last_case_and_deaths_by_100000_habitants(df, city_name):
+    ax = (
+        (100000 * df["ultimos_confirmados"] / df["populacao_estimada"])
+        .rolling(7, center=True)
+        .mean()
+        .plot(style="-", figsize=FS, logy=True, alpha=0.6)
+    )
+    ax = (
+        (100000 * df["ultimas_mortes"] / df["populacao_estimada"])
+        .rolling(7, center=True)
+        .mean()
+        .plot(style="-", ax=ax, logy=True, alpha=0.6)
+    )
+    ax.grid()
+    _ = ax.set(
+        title=f"COVID-19 diário (por 100.000 habitantes) na cidade de {city_name} (escala logarítmica)",
+        ylabel="Escala em Log",
+    )
+    _ = ax.legend(["Últimos casos confirmados diários", "Últimas mortes diárias"])
+    ax.autoscale(enable=True, axis="x", tight=True)
+
+#%% Filtro, otimização e plot dos valores na cidade de Araucária
+
+first_city = query_df_city.iat[0, 0]
+df_first = filter_and_format_by_city(first_city)
+plot_df_by_last_case_and_deaths_by_100000_habitants(df_first, first_city)
+
+#%% Filtro, otimização e plot dos valores na cidade de Quitandinha
+
+second_city = query_df_city.iat[1, 0]
+df_second = filter_and_format_by_city(second_city)
+plot_df_by_last_case_and_deaths_by_100000_habitants(df_second, second_city)
+
+#%% Filtro, otimização e plot dos valores na cidade de Uberlândia
+
+third_cirty = query_df_city.iat[2, 0]
+df_third = filter_and_format_by_city(third_cirty)
+plot_df_by_last_case_and_deaths_by_100000_habitants(df_third, third_cirty)
+
+#%% Filtro, otimização e plot dos valores na cidade de Canoinhas
+
+fourth_city = query_df_city.iat[3, 0]
+df_fourth = filter_and_format_by_city(fourth_city)
+plot_df_by_last_case_and_deaths_by_100000_habitants(df_fourth, fourth_city)
+
+
+#%% Filtro, otimização e plot dos valores na cidade de Gramado
+
+fifth_city = query_df_city.iat[4, 0]
+df_fifth = filter_and_format_by_city(fifth_city)
+plot_df_by_last_case_and_deaths_by_100000_habitants(df_fifth, fifth_city)
+
+
+#%% Otimização por Data de todos os valores
+query_df_new = query_df
+query_df_new.data = pd.to_datetime(query_df_new.data, format="%Y-%m-%d")
+query_df_new.set_index("data", inplace=True)
+query_df_new.index.name = "Data"
 print(query_df)
 
-#%%
+#%% Cálculo das ultimas mortes por população com todos os valores da otimização a cada 100000 habitantes
+
+covid_rate_all_cities = query_df_new[["nome", "ultimas_mortes", "populacao_estimada"]]
+covid_rate_all_cities["covid_rate"] = 100000 * covid_rate_all_cities['ultimas_mortes'] / covid_rate_all_cities["populacao_estimada"]
+covid_rate_all_cities.drop(["populacao_estimada", "ultimas_mortes"], axis=1, inplace=True)
+
+covid_rate_all_cities = covid_rate_all_cities.pivot_table(index="Data", columns="nome", values="covid_rate")
+
+#%% Plot agregado das cidades
+ax = covid_rate_all_cities.rolling(7, center=True).mean().plot(figsize=FS, alpha=0.6)
+ax.grid()
+_ = ax.set(
+    title="COVID-19 diário (por 100.000) nos Munícipios escolhidos",
+    ylabel="Total de mortes COVID-19",
+)
+ax.autoscale(enable=True, axis="x", tight=True)
+
+
+
+
+
